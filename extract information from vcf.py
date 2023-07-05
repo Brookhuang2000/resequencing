56e95e156879b1602c3eeb64e31b61ace8f7d40e
Brookhuang2000 <118552588+Brookhuang2000@users.noreply.github.com>
2023-07-05 15:44:25 +0800

    Update extract information from vcf.py

--- extract information from vcf.py
+++ extract information from vcf.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 
-#Replace with your file name
-filename = "output.vcf"
+import pandas as pd
 
-#Open the file and read its contents.
-with open(filename, "r") as file:
-  lines = file.readlines()
+# 读取文件
+df = pd.read_csv('output.vcf', delimiter='\t', skiprows=2692)
+
+# 提取所需的列
+columns_to_extract = ['#CHROM', 'POS', 'ID', 'REF', 'QUAL', 'FILTER', 'INFO', 'FORMAT']
+
+alt_columns = [col for col in df.columns if 'ALT' in col]
+byb_columns = [col for col in df.columns if 'BYB' in col]
+
+columns_to_extract += alt_columns + byb_columns
+
+extracted_data = df[columns_to_extract]
+
+# 将提取的数据写入新文件
+extracted_data.to_csv('3result.vcf', sep='\t', index=False)
+
+print("数据已成功写入 3result.vcf 文件。")
 
